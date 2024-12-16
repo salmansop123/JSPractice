49e2e4304587fc44be684254b48aa3e12d37cc59
Salman Mazhar <salmansop123@gmail.com>
2024-12-16 17:50:36 +0500

    text added

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-alert("Hello world!")
-console.log("Git add")
+// Practice set 2
+// Question 01
+let marks = {
+    salman: 90,
+    ahsan: 80,
+    afaq: 100,
+    mujhtaba: 70,
+    ali: 33
+}
+
+// for (let i=0;i<Object.keys(marks).length;i++){
+//     console.log("Marks of " + Object.keys(marks)[i] + " are " + marks[Object.keys(marks)[i]]);
+// }
+
+// Question 02 using for in loop
+for (let i in marks){
+    console.log("Marks of " + i + " are " + marks[i]);
+}
